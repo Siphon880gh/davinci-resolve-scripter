8baa1d38c37e2afed4cbb897c679c7ca1b73455f
Siphon880gh <weng.f.fung@gmail.com>
2024-09-17 04:28:00 -0700

    Rebranding

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Weng's DaVinci Scripter</title>
+    <title>DaVinci Resolve Scripter</title>
 
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
