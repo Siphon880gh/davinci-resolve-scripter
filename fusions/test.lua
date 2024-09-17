e37437f2263258a371fd92d911b0cc5f9cace001
Siphon880gh <weng.f.fung@gmail.com>
2024-09-17 02:26:10 -0700

    Initial layout

---
+++ fusions/test.lua
@@ -0,0 +1,231 @@
+{
+	Tools = ordered() {
+		Group1 = GroupOperator {
+			CtrlWZoom = false,
+			Inputs = ordered() {
+				Comments = Input { Value = "Before pasting into Fusion:\n- Adjust frame points with fps x clip duration. \nCurrently set to 115 (5 seconds).\n\nAfter pasting into Fusion:\n- Connect MediaIn to INPUT_MED_IN\n- Connect OUTPUT_MED_OUT to MediaOut", },
+				Input1 = InstanceInput {
+					SourceOp = "INPUT_MED_IN",
+					Source = "Background",
+				}
+			},
+			Outputs = {
+				Output1 = InstanceOutput {
+					SourceOp = "OUTPUT_MED_OUT",
+					Source = "Output",
+				}
+			},
+			ViewInfo = GroupInfo {
+				Pos = { 302.5, 106.864 },
+				Flags = {
+					Expanded = true,
+					AllowPan = false,
+					GridSnap = true,
+					AutoSnap = true,
+					RemoveRouters = true
+				},
+				Size = { 621, 198.364, 310.5, 24.2424 },
+				Direction = "Horizontal",
+				PipeStyle = "Direct",
+				Scale = 1,
+				Offset = { -302.5, -106.864 }
+			},
+			Tools = ordered() {
+				OUTPUT_MED_OUT = Merge {
+					NameSet = true,
+					Inputs = {
+						Background = Input {
+							SourceOp = "Merge1",
+							Source = "Output",
+						},
+						PerformDepthMerge = Input { Value = 0, }
+					},
+					ViewInfo = OperatorInfo { Pos = { 550, 181.5 } },
+				},
+				Merge1 = Merge {
+					Inputs = {
+						Background = Input {
+							SourceOp = "GaussianBlur1",
+							Source = "Output",
+						},
+						Foreground = Input {
+							SourceOp = "TransformZoomIn",
+							Source = "Output",
+						},
+						PerformDepthMerge = Input { Value = 0, }
+					},
+					ViewInfo = OperatorInfo { Pos = { 385, 247.5 } },
+				},
+				GaussianBlur1 = ofx.com.blackmagicdesign.resolvefx.GaussianBlur {
+					Inputs = {
+						Source = Input {
+							SourceOp = "INPUT_MED_IN",
+							Source = "Output",
+						},
+						HStrength = Input { Value = 1, },
+						VStrength = Input { Value = 0.400000005960464, },
+						Gang = Input { Value = 1, },
+						advancedControlsGroup = Input { Value = 1, },
+						BorderType = Input { Value = FuID { "BORDER_TYPE_REPLICATE" }, },
+						isBlurAlpha = Input { Value = 1, },
+						BlendAmount = Input { Value = 0, },
+						blendGroup = Input { Value = 0, },
+						blendIn = Input { Value = 1, },
+						blend = Input { Value = 0, },
+						ignoreContentShape = Input { Value = 0, },
+						legacyIsProcessRGBOnly = Input { Value = 0, },
+						IsNoTemporalFramesReqd = Input { Value = 0, },
+						refreshTrigger = Input { Value = 1, },
+						srcProcessingAlphaMode = Input { Value = 1, },
+						dstProcessingAlphaMode = Input { Value = 1, },
+						resolvefxVersion = Input { Value = "3.0", }
+					},
+					ViewInfo = OperatorInfo { Pos = { 220, 247.5 } },
+				},
+				TransformTranslate = Transform {
+					NameSet = true,
+					Inputs = {
+						Center = Input {
+							SourceOp = "Path1",
+							Source = "Position",
+						},
+						Input = Input {
+							SourceOp = "INPUT_MED_IN",
+							Source = "Output",
+						}
+					},
+					ViewInfo = OperatorInfo { Pos = { 385, 115.5 } },
+				},
+				TransformZoomIn = ofx.com.blackmagicdesign.resolvefx.Transform {
+					NameSet = true,
+					Inputs = {
+						Source = Input {
+							SourceOp = "TransformTranslate",
+							Source = "Output",
+						},
+						controlMode = Input { Value = FuID { "TransformControlsSliders" }, },
+						controlReset = Input { Value = 0, },
+						controlGroup = Input { Value = 1, },
+						controlVisibility = Input { Value = FuID { "Show" }, },
+						posX = Input {
+							SourceOp = "TransformZoomInPositionX",
+							Source = "Value",
+						},
+						posY = Input {
+							SourceOp = "TransformZoomInPositionY",
+							Source = "Value",
+						},
+						zoom = Input {
+							SourceOp = "TransformZoomInZoom",
+							Source = "Value",
+						},
+						rotate = Input { Value = 0, },
+						scaleX = Input { Value = 1, },
+						scaleY = Input { Value = 1, },
+						pitch = Input { Value = 0, },
+						yaw = Input { Value = 0, },
+						flipH = Input { Value = 0, },
+						flipV = Input { Value = 0, },
+						adjustGroup = Input { Value = 0, },
+						isCrop = Input { Value = 0, },
+						cropL = Input { Value = 0, },
+						cropR = Input { Value = 0, },
+						cropT = Input { Value = 0, },
+						cropB = Input { Value = 0, },
+						edgeSoftness = Input { Value = 0, },
+						edgeRounding = Input { Value = 0, },
+						animationGroup = Input { Value = 0, },
+						serializedWarpable = Input {
+							Value = Text {
+							},
+						},
+						serializedPinnable = Input {
+							Value = Text {
+							},
+						},
+						motionBlur = Input { Value = 0, },
+						advancedGroup = Input { Value = 0, },
+						edgeBehaviour = Input { Value = FuID { "Constant" }, },
+						CompositeType = Input { Value = FuID { "COMPOSITE_NORMAL" }, },
+						olayAntiAlias = Input { Value = 1, },
+						previewAlpha = Input { Value = 0, },
+						isLegacyCrop = Input { Value = 0, },
+						isLegacyAlphaHandling = Input { Value = 0, },
+						isEnforceBlanking = Input { Value = 0, },
+						blendGroup = Input { Value = 0, },
+						blendIn = Input { Value = 1, },
+						blend = Input { Value = 0, },
+						ignoreContentShape = Input { Value = 0, },
+						legacyIsProcessRGBOnly = Input { Value = 0, },
+						IsNoTemporalFramesReqd = Input { Value = 0, },
+						refreshTrigger = Input { Value = 1, },
+						srcProcessingAlphaMode = Input { Value = -1, },
+						dstProcessingAlphaMode = Input { Value = -1, },
+						resolvefxVersion = Input { Value = "1.4", }
+					},
+					ViewInfo = OperatorInfo { Pos = { 385, 181.5 } },
+				},
+				INPUT_MED_IN = Merge {
+					NameSet = true,
+					Inputs = {
+						PerformDepthMerge = Input { Value = 0, }
+					},
+					ViewInfo = OperatorInfo { Pos = { 55, 181.5 } },
+				}
+			},
+		},
+		Path1 = PolyPath {
+			DrawMode = "InsertAndModify",
+			CtrlWZoom = false,
+			Inputs = {
+				Displacement = Input {
+					SourceOp = "Path1Displacement",
+					Source = "Value",
+				},
+				PolyLine = Input {
+					Value = Polyline {
+						Points = {
+							{ Linear = true, LockY = true, X = 0, Y = 0, RX = 0.0666666666666667, RY = 0.0666666666666667 },
+							{ Linear = true, LockY = true, X = 0.2, Y = 0.2, LX = -0.0666666666666667, LY = -0.0666666666666667, RX = 0, RY = 0 },
+							{ Linear = true, LockY = true, X = 0.2, Y = 0.2, LX = 0, LY = 0 }
+						}
+					},
+				}
+			},
+		},
+		Path1Displacement = BezierSpline {
+			SplineColor = { Red = 255, Green = 0, Blue = 255 },
+			NameSet = true,
+			KeyFrames = {
+				[0] = { 0, RH = { 38.3333333333333, 0.333333333333333 }, Flags = { LockedY = true } },
+				[_END_FRAME_] = { 1, LH = { 117.666666666667, 1 }, Flags = { Linear = true, LockedY = true } }
+			}
+		},
+		TransformZoomInPositionX = BezierSpline {
+			SplineColor = { Red = 237, Green = 132, Blue = 222 },
+			NameSet = true,
+			KeyFrames = {
+				[0] = { 0, RH = { 38.3333333333333, -0.0666666666666667 }, Flags = { Linear = true } },
+				[_END_FRAME_] = { -0.2, LH = { 76.6666666666667, -0.133333333333333 }, Flags = { Linear = true } }
+			}
+		},
+		TransformZoomInPositionY = BezierSpline {
+			SplineColor = { Red = 237, Green = 132, Blue = 54 },
+			NameSet = true,
+			KeyFrames = {
+				[0] = { 0, RH = { 38.3333333333333, -0.0666666666666667 }, Flags = { Linear = true } },
+				[_END_FRAME_] = { -0.2, LH = { 76.6666666666667, -0.133333333333333 }, Flags = { Linear = true } }
+			}
+		},
+		TransformZoomInZoom = BezierSpline {
+			SplineColor = { Red = 254, Green = 144, Blue = 123 },
+			CtrlWZoom = false,
+			NameSet = true,
+			KeyFrames = {
+				[0] = { 1, RH = { 38.3333333333333, 1.13333333333333 }, Flags = { Linear = true } },
+				[_END_FRAME_] = { 1.4, LH = { 76.6666666666667, 1.26666666666667 }, Flags = { Linear = true } }
+			}
+		}
+	},
+	ActiveTool = "Group1"
+}
---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weng's DaVinci Scripter</title>
+    <link href="//cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script>
+        function hydrate() {
+            document.querySelectorAll('.copy-icon').forEach(function(icon) {
+                icon.addEventListener('click', function(event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+
+                    // Get the corresponding content
+                    var content = this.closest(".card").querySelector(".card-text").textContent;
+
+                    // Create a temporary input element to hold the content
+                    var tempInput = document.createElement('textarea');
+                    tempInput.value = content;
+                    document.body.appendChild(tempInput);
+
+                    // Select the text and copy it
+                    tempInput.select();
+                    document.execCommand('copy');
+
+                    // Remove the temporary input element
+                    document.body.removeChild(tempInput);
+                    
+                    // Close the details element
+                    const details = this.closest("details")
+                    details.open = false;
+                    $(details).removeClass("bg-gray-200").addClass("bg-gray-100");
+
+                    // Provide feedback (optional)
+                    // alert('Content copied to clipboard!');
+
+                    // Show the "Copied to clipboard" notification
+                    var notification = document.getElementById('copy-notification');
+                    notification.classList.remove('hidden');
+
+                    // Hide the notification after 2 seconds
+                    setTimeout(function() {
+                        notification.classList.add('hidden');
+                    }, 2000);
+
+                }); // click
+            }); // forEach
+        } // hydrate
+
+        let template = "";
+
+        fetch("./fusions/test.lua")
+        .then(response=>response.text())
+        .then(response => {
+            template = response;
+
+            // Presets into webpage
+            [
+                ["blc", 23, 5],
+                ["blc", 23, 5]
+            ].forEach((clipData, index) => {
+                const [preset, clipFps, clipSeconds] = clipData;
+                const clipTemplate = template
+                    .replaceAll("_END_FRAME_", parseInt(clipSeconds*clipFps));
+                console.log(clipTemplate);
+                
+                $("#results").append(`<details class="bg-gray-200 p-4 m-4">
+                    <summary>${clipData.toString()}</summary>
+
+                    <div class="container mt-5">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between align-items-center inline w-fit border border-gray-300 w-fit" style="width:fit-content !important;">
+                                <i class="fas fa-copy text-3xl copy-icon" style="cursor: pointer;" title="Copy to clipboard"></i>
+                            </div>
+                            <div class="card-body">
+                                <code id="card-content" class="card-text whitespace-pre-wrap">${clipTemplate}</code>
+                            </div>
+                        </div>
+                    </div>
+
+                </details>
+                `); // append
+
+                hydrate();
+
+            });
+        });
+    </script>
+</head>
+<body>
+    <div class="container mt-4">
+        <h1>Weng's DaVinci Scripter</h1>
+        <main id="results"></main>
+    </div>
+
+    <!-- Notification -->
+    <div id="copy-notification" class="hidden fixed bottom-4 right-4 bg-green-100 text-green-600 px-4 py-2 rounded shadow-lg opacity-70">
+        Copied to clipboard!
+    </div>
+</body>
+</html>
