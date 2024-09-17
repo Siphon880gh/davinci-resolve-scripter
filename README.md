defe4fcf4c9678172d122496112a31d66e116a5a
Siphon880gh <weng.f.fung@gmail.com>
2024-09-17 04:06:37 -0700

    Instructions

---
+++ README.md
@@ -0,0 +1,7 @@
+# DaVinci Resolve Scripter
+
+By Weng (Weng Fei Fung). Script that automates zoom and pan effects.
+
+## Usage
+
+Run the php file. Copy to each consecutive fusion clip in DaVinci Resolve.
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weng's DaVinci Scripter</title>
-    <link href="//cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -98,13 +98,24 @@
 </head>
 <body>
     <div class="container mt-4">
-        <h1>Weng's DaVinci Scripter</h1>
+        <h1>DaVinci Resolve Scripter</h1>
+
+        <div class="author-line mv-2">
+            <b>By Weng (Weng Fei Fung)</b>
+            <a class="px-1" href="https://github.com/Siphon880gh/" target="_blank"><i class="fab fa-github"></i></a>
+            <a class="px-1" href="https://www.linkedin.com/in/weng-fung/" target="_blank"><i class="fab fa-linkedin"></i></a>
+            <a class="px-1" href="https://www.youtube.com/@WayneTeachesCode/" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+
+        <p class="my-4">Script that automates zoom and pan effects.</p>
+
         <main id="results"></main>
     </div>
 
     <!-- Notification -->
     <div id="copy-notification" class="hidden fixed bottom-4 right-4 bg-green-100 text-green-600 px-4 py-2 rounded shadow-lg opacity-70">
-        Copied to clipboard!
+        Copied to clipboard!<br/>
+        Paste into Fusion Editor
     </div>
 </body>
 </html>
