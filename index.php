5a5477cac185ce1ddf1a80db859f2383f0656f26
Siphon880gh <weng.f.fung@gmail.com>
2024-09-18 21:06:24 -0700

    Show all effects

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <script>
-        const clipCount = 8;
+        const clipCount = 12;
         const cyclicalMotionEffects = [
             "zoom140to100",
             "zoom120to100",
