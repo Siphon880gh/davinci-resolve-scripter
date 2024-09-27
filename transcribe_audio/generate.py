6b76350a08c31f550532162eb4492252cea673e6
Siphon880gh <weng.f.fung@gmail.com>
2024-09-27 03:08:38 -0700

    Transcribe audio with more options

--- transcribe_audio/generate.py
+++ transcribe_audio/generate.py
@@ -17,10 +17,34 @@ try:
     # Word formatting options
     # highlight_words: Underline each word as it is spoken in srt and vtt
     # More options at: https://github.com/openai/whisper/blob/main/whisper/transcribe.py
+    # 1/15 = 0-1 secs per subtitle screen
+    # 2/15 = 1 secs per subtitle screen
+    # 3/15 = 1-2 secs per subtitle screen
+    # 1/30 = 1-2 secs per subtitle screen
+    # 2/30 = 2-3 secs per subtitle screen
+    # 3/30 = 3-4 secs per subtitle screen
+    # 1/40 = 1-2 secs per subtitle screen
+    # 2/40 = 3-4 secs per subtitle screen
+    # 3/40 = 6 secs per subtitle screen
+    # 1/45 = 1-2 secs per subtitle screen
+    # 2/45 = 3-4 secs per subtitle screen
+    # 3/45 = 6 secs per subtitle screen
+    # 1/50 = 2 secs per subtitle screen
+    # 2/50 = 4 secs per subtitle screen
+    # 3/50 = 6-7 secs per subtitle screen
+    # 1/55 = 2-3 secs per subtitle screen
+    # 2/55 = 4-5 secs per subtitle screen
+    # 3/55 = 7-8 secs per subtitle screen
+    # 1/60 = 2-3 secs per subtitle screen
+    # 2/60 = 6-7 secs per subtitle screen
+    # 3/60 = 10-13 secs per subtitle screen
+    # 1/65 = 3-4 secs per subtitle screen
+    # 2/65 = 7-9 secs per subtitle screen
+    # 3/65 = 11-13 secs per subtitle screen
     word_formatting = {
         "highlight_words": False,
-        "max_line_count": 2,
-        "max_line_width": 15
+        "max_line_count": 3,
+        "max_line_width": 65
     }
 
     write_to_dir = get_writer("srt", os.path.dirname(OUTPUT_DIR))
